1cb41b4c60cac40b962330959ec568f62cd02e75
Christian Pisalbon <pisalbondaniel24@gmail.com>
2025-04-28 21:02:57 +0800

    apply some functions

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+    //Toggle mobile menu visibility//
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+
+});
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+
